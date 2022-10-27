ccd43ca1eb59647a632ed63458084c12ecf4175a
jinqkuenlim <jinqkuen@apppay.tech>
2022-10-27 14:12:36 +0800

    bugfix | corect payment success wording

--- resources/views/paySuccess.blade.php
+++ resources/views/paySuccess.blade.php
@@ -8,8 +8,8 @@ $active = 'pay'
     <div class="hero-content text-center">
         <div class="max-w-md">
             <h1 class="text-5xl font-bold">Payment Success</h1>
-            <p class="py-6">You have completed your payment. You may go to My Bills page to check it out.</p>
-            <a class="btn btn-primary"  href="/bills">My Bills</a>
+            <p class="py-6">You have completed your payment. You may go to My Bills page to get your payment receipt.</p>
+            <a class="btn btn-primary"  href="/receipts">My Bills</a>
         </div>
     </div>
 </div>
